6e3232e1935caa38449010b8e9675236cbfcdeb5
Chris Engelhard <c.engelhard@digi4care.nl>
2024-07-04 21:45:33 +0200

    removed wrong paths

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-export { default as GoogleAnalytics } from './src/GoogleAnalytics.astro';
-export { default as GoogleAnalyticsNoscript } from './src/GoogleAnalyticsNoscript.astro';
 export { default as GoogleTagmanager } from './src/GoogleTagmanager.astro';
 export { default as GoogleTagmanagerNoscript } from './src/GoogleTagmanagerNoscript.astro';
