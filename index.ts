50498863644e7a1d8ea23b7fb76cdca4263d1f19
Chris Engelhard <chris@chrisengelhard.nl>
2026-05-15 16:10:33 +0200

    fix: duplicate listener guard for astro:after-swap, biome migrate, gitignore update

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-export { default as GoogleTagmanager } from './src/GoogleTagmanager.astro';
-export { default as GoogleTagmanagerPartytown } from './src/GoogleTagmanagerPartytown.astro';
-export { default as GoogleTagmanagerNoscript } from './src/GoogleTagmanagerNoscript.astro';
-export { default as SiteVerification } from './src/SiteVerification.astro';
+export { default as GoogleTagmanager } from "./src/GoogleTagmanager.astro";
+export { default as GoogleTagmanagerNoscript } from "./src/GoogleTagmanagerNoscript.astro";
+export { default as GoogleTagmanagerPartytown } from "./src/GoogleTagmanagerPartytown.astro";
+export { default as SiteVerification } from "./src/SiteVerification.astro";
